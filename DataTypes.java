1de02653098c0a4d6842e5a46a9a4c0c2ce3981a
Nurul-Afifah <nurullafifahh1112@gmail.com>
2020-04-24 13:20:23 +0700

    Input dengan Scanner

--- DataTypes.java
+++ DataTypes.java
@@ -1,24 +1,40 @@
 import javax.swing.JOptionPane;
+import java.util.Scanner;
 
 public class DataTypes{
  public static void main(String[] args){
 
- String namaDepan = "Nurul";
- String namaBelakang = "Afifah";
- int usia = 18;
- int targetTahunKuliah = 4;
- double ipk = 3.89764512;
- char nilaiAbjad = 'A';
- boolean cantik = true;
+  Scanner scanner = new Scanner(System.in);
 
- System.out.println("Nama depan : " + namaDepan);
- System.out.println("Nama belakang : " + namaBelakang);
- System.out.println("Usia : " + usia);
- System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
- System.out.println("IPK : " + ipk);
- System.out.println("Nilai PBO : " + nilaiAbjad);
- System.out.println("Cantik : " + cantik);
+  String namaDepan = "Nurul";
+  String namaBelakang = "Afifah";
+  int usia = 18;
+  int targetTahunKuliah = 4;
+  double ipk = 3.89764512;
+  char nilaiAbjad = 'A';
+  boolean cantik = true;
 
- JOptionPane.showMessageDialog(null,"Hai, " + namaDepan + namaBelakang);
+  System.out.print("Input Nama Depan : ");
+  namaDepan = scanner.nextLine();
+  System.out.print("Input Usia : ");
+  usia = scanner.nextInt();
+  System.out.print("Input IPK : ");
+  ipk = scanner.nextDouble();
+  System.out.print("");
+  System.out.print("Input Nilai Abjad : ");
+  nilaiAbjad = scanner.next().charAt(0);
+  System.out.print("Cantik? : ");
+  cantik = scanner.nextBoolean();
+
+  System.out.println("=========== OUTPUT ===========");
+  System.out.println("Nama depan : " + namaDepan);
+  System.out.println("Nama belakang : " + namaBelakang);
+  System.out.println("Usia : " + usia);
+  System.out.println("Target Kuliah : " + targetTahunKuliah + " tahun");
+  System.out.println("IPK : " + ipk);
+  System.out.println("Nilai PBO : " + nilaiAbjad);
+  System.err.println("Cantik : " + cantik);
+
+  JOptionPane.showMessageDialog(null,"Hai, " + namaDepan + namaBelakang);
  }
 }
